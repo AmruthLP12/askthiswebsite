2066bcd2180008c1b44f1856d725abb90aec38c8
Amruth L P <134767358+AmruthLP12@users.noreply.github.com>
2024-08-21 21:57:03 +0530

    UI for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,29 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-   <div className="">This will only work when you add website next to the deployed link
-   
-   <div>For example 
-    <a href="#" className=""></a>
-   </div>
-   </div>
+    <div className="bg-zinc-900 text-white flex items-center justify-center min-h-screen">
+      <div className="bg-zinc-800 p-8 rounded-lg shadow-lg max-w-xl mx-auto">
+        <h1 className="text-2xl font-bold mb-4 text-center">
+          Welcome to AskThisWebsite
+        </h1>
+        <p className="text-lg font-semibold text-center">
+          This will only work when you add 'website' next to the deployed link.
+        </p>
+        <p className="text-sm text-zinc-400 text-center mt-2">
+          i.e. https://askthiswebsite-cwa.vercel.app
+        </p>
+        <div className="mt-6 text-center">
+          <p className="text-sm text-zinc-400">
+            For example:
+            <a
+              href="https://askthiswebsite-cwa.vercel.app/https:/www.framer.com/motion/introduction"
+              className="text-blue-500 hover:text-blue-400 ml-2 underline"
+              target="_blank"
+            >
+              Click Here
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
